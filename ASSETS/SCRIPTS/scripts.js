7637701047e443da3fe24e59f7f9d6d0cd64989e
pegw2 <peterw1502@gmail.com>
2023-06-23 00:48:21 +0100

    e

--- ASSETS/SCRIPTS/scripts.js
+++
@@ -1,8 +0,0 @@
-let rotate = 0;
-let rotateAmount = 90
-
-function logoClick () {
-    const logo = document.getElementById('headerLogo')
-    rotate = (rotate + rotateAmount) % 360;
-    logo.style.transform = 'rotate(' + rotate + 'deg)';
-}
